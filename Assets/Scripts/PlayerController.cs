83a94096a868dc7e7dd432a6aac0b9b967c58b6b
Anton Kurenkov <quantum0@mail.ru>
2023-03-18 18:14:21 +0300

    WiP

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,6 +20,8 @@ public class PlayerController : MonoBehaviour
         float h = hSpeed * Input.GetAxis("Horizontal");
         float v = vSpeed * Input.GetAxis("Vertical");
 
-        rb.AddForce(new Vector3(h, 0, v));        
+        rb.AddForce(new Vector3(h, 0, v));
+
+        rb.AddForce(-rb.velocity.normalized*1.5f);      
     }
 }
