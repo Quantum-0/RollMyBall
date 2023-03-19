db46597dee941dfb01caf62b4e811a3a073e6397
Anton Kurenkov <quantum0@mail.ru>
2023-03-19 17:25:23 +0300

    Update scene and rework controller script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BallMaterialType
+{
+    Rock,
+    Wood,
+    Paper,
+}
+
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float Speed = 3;
-    [SerializeField] float AirFriction = 1.5f;
+    [SerializeField] float Speed = 4.5f;
+    [SerializeField] float AirFriction = 1.3f;
+    [SerializeField] float MaxAngularVelocity = 15f;
+    [SerializeField] GameObject paperForm;
 
     Rigidbody rb;
+
+    [SerializeField] BallMaterialType materialType;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();        
+        rb = GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = MaxAngularVelocity;
+        ApplyMaterial();
     }
 
     // Update is called once per frame
@@ -21,10 +35,66 @@ public class PlayerController : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        rb.AddForce(new Vector3(h,0,v).normalized * Speed);
+        rb.AddTorque(new Vector3(v, 0, -h).normalized * Speed);
 
         var flattenedVelocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
 
         rb.AddForce(-flattenedVelocity*AirFriction);      
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            materialType = materialType.Next();
+            ApplyMaterial();
+        }
+    }
+
+    public void ApplyMaterial()
+    {
+        Debug.Log("Switch material type to " + materialType.ToString());
+        switch (materialType)
+        {
+            case BallMaterialType.Rock:
+                rb.mass = 2.75f;
+                Speed = 4.5f;
+                AirFriction = 1.3f;
+                paperForm.SetActive(false);
+                return;
+            case BallMaterialType.Wood:
+                rb.mass = 1.55f;
+                Speed = 4.5f;
+                AirFriction = 1.3f;
+                paperForm.SetActive(false);
+                return;
+            case BallMaterialType.Paper:
+                rb.mass = 0.3f;
+                Speed = 2f;
+                AirFriction = 0.2f;
+                paperForm.SetActive(true);
+                return;
+            default:
+                break;
+        }
+    }
+}
+
+
+public static class ExtensionMethods
+{
+    public static BallMaterialType Next(this BallMaterialType myEnum)
+    {
+        switch (myEnum)
+        {
+            case BallMaterialType.Rock:
+                return BallMaterialType.Wood;
+            case BallMaterialType.Wood:
+                return BallMaterialType.Paper;
+            case BallMaterialType.Paper:
+                return BallMaterialType.Rock;
+            default:
+                throw new Exception();
+        }
     }
 }
