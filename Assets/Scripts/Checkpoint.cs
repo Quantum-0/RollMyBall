34d2dd09992ed07fa9fb38d1561b66b4090b29fe
Anton Kurenkov <quantum0@mail.ru>
2023-05-27 19:54:04 +0300

    Add free particle system asset from Hovl Studio for checkpoints, start and finish

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -10,30 +10,30 @@ public class Checkpoint : MonoBehaviour
     public GameObject player;
     void Start()
     {
+        var fires = this.GetComponentsInChildren<ParticleSystem>();
+        foreach (var ps in fires)
+            ps.enableEmission = false;
         player = GameObject.FindGameObjectWithTag("Player");
         CheckpointsLeft.Add(this);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("COLLISION");
         if (Activated)
             return;
         if (other.gameObject != player)
             return;
 
-        var ldcc = other.GetComponent<LivesDeathAndCheckpointController>();
+        var ldcc = player.GetComponent<LivesDeathAndCheckpointController>();
         ldcc.LastCheckpoint = this.gameObject;
         CheckpointsLeft.Remove(this);
         Activated = true;
 
+        var fires = this.GetComponentsInChildren<ParticleSystem>();
+        foreach (var ps in fires)
+            ps.enableEmission = true;
+
         // TODO: Звук сохранения на чекпоинте (записать с помощью глюкофона?)
         // TODO: Зажечь на нём огоньки (патикл системы заюзать)
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
